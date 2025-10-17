455c98759cd3fa84a214704d7585ff4bbb5fd8e3
Tanggogrich <vakhtanggogrich@gmail.com>
2025-10-17 17:27:14 +0200

    bugfix 2.1

--- src/main/java/monitoring/Main.java
+++ src/main/java/monitoring/Main.java
@@ -45,7 +45,7 @@ public class Main {
 
         DashboardWrapper dashboardWrapper = new DashboardWrapper(
                 "dashboard.grafana.app/v1beta1",
-                "Dashboards",
+                "dashboard",
                 new Metadata("my-dashboard"),
                 dashboard);
 
